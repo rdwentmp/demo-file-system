f865eb3d7cefb96f624656326a30eea07efaae07
rdwentmp <maaarek7823@gmail.com>
2021-08-09 10:23:08 +0200

    creating imutable structures part1

--- src/com/rtjvm/scala/oop/commands/Mkdir.scala
+++ src/com/rtjvm/scala/oop/commands/Mkdir.scala
@@ -1,4 +1,5 @@
 package com.rtjvm.scala.oop.commands
+import com.rtjvm.scala.oop.files.Directory
 import com.rtjvm.scala.oop.filesystem.State
 
 class Mkdir(name: String) extends Command {
@@ -7,6 +8,20 @@ class Mkdir(name: String) extends Command {
     val wd = state.wd
     if (wd.hasEntry(name)) {
       state.setMessage("Entry " + name + " already exists!")
+    } else if (name.contains(Directory.SEPARATOR)) {
+      state.setMessage(name + " must not contain separators!")
+    } else if (checkIllegal(name)) {
+      state.setMessage(name + ": illegal entry name!")
+    } else {
+      doMkdir(state, name)
     }
+  }
+
+  def checkIllegal(name: String): Boolean = {
+    name.contains(".")
+  }
+
+  def doMkdir(state: State, name: String): State = {
+    ???
   }
 }
--- src/com/rtjvm/scala/oop/files/Directory.scala
+++ src/com/rtjvm/scala/oop/files/Directory.scala
@@ -3,6 +3,8 @@ package com.rtjvm.scala.oop.files
 class Directory(override val parentPath: String, override val name: String, val contents: List[DirEntry])
   extends DirEntry(parentPath, name) {
 
+  def hasEntry(name: String): Boolean = ???
+
 }
 
 object Directory {
